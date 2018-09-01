2816f3b837b46c6333882aa2800e458e2d08088d
sentu.biswas013 <sentu.biswas13@gmail.com>
2018-09-02 00:57:21 +0530

    sfsd

---
+++ 02-AngularJs2/js/script.js
@@ -0,0 +1,13 @@
+var app = angular.module('docsTemplateUrlDirective', [])
+app.controller('Controller', function($scope) {
+	$scope.name = "Sentu Biswas";
+  /*$scope.customer = {
+    name: 'Naomi',
+    address: '1600 Amphitheatre'
+  };*/
+})
+/*app.directive('myCustomer', function() {
+  return {
+    templateUrl: 'my-customer.html'
+  };
+});*/
